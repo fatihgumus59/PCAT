9b2febc1b4532f8e1fbcf20e47a152d2bf2a2598
fatihgumus59 <fatihgumus72@gmail.com>
2022-02-04 16:42:50 +0300

    image uploaded

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
 
 const path = require('path');
 const ejs = require('ejs');
+const fs = require('fs');
 
 const Photos = require('./models/Photo');
 
@@ -17,9 +19,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(fileUpload());
 
 app.get('/', async (req, res) => {
-  const photos = await Photos.find({});
+  const photos = await Photos.find({}).sort('-dataCreated')
   res.render('index', {
     photos,
   });
@@ -41,8 +44,23 @@ app.get('/add', (req, res) => {
 });
 
 app.post('/photos', async (req, res) => {
-  await Photos.create(req.body);
-  res.redirect('/');
+  const uploadDir = 'public/uploads';
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadImage.name;
+
+  uploadImage.mv(uploadPath, async () => {
+    await Photos.create({
+      ...req.body,
+      image: '/uploads/' + uploadImage.name,
+    });
+    res.redirect('/');
+  });
+
 });
 
 const port = 3000;
