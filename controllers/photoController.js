6ca7ac1c6d6bc1c4049c4c2a953ccc1b96db751f
fatihgumus59 <fatihgumus72@gmail.com>
2022-02-08 21:44:22 +0300

    add pagination

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -2,9 +2,19 @@ const Photos = require('../models/Photo');
 const fs = require('fs');
 
 exports.getAllPhotos = async (req, res) => {
-  const photos = await Photos.find({}).sort('-dataCreated');
+  const page = req.query.page || 1; // başlangıç sayfası
+  const photoPerPage = 3; // her sayfada 3 foto göster
+  const totalPhoto = await Photos.find({}).countDocuments(); // tüm fotolar sayısı
+
+  const photos = await Photos.find({}) // tüm fotoları aldık
+    .sort('-dataCreated') // sıraladık
+    .skip((page - 1) * photoPerPage) // sayfa numarlandırma
+    .limit(photoPerPage);
+
   res.render('index', {
     photos,
+    current: page, // seçili sayfa
+    pages: Math.ceil((totalPhoto / photoPerPage)),  // toplam sayfa sayısı 
   });
 };
 
